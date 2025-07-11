078ac3cf5c8f161f704b57655b1fe0183708fc67
coconutDreams-56 <cabreraheily@gmail.com>
2025-07-10 20:48:45 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
-#
+/*** Project A Problem 7.6, Chapter 7
+*--------------------------------------------- ***** 
+Modify Programming Project 7.20.3 so that you first print the lines in the order that they were entered, then print them in sorted order. 
+***** When you sort the lines, only rearrange the pointers in the lines array. Write a program that reads lines of text and appends them to a char buffer[1000]. */
+
+// Programmer: Heily Cabrera // Tester: Ashley Jacobson
+
+#include <iostream>
+#include <stream>
+using namespace std;
+
+int main()
+
+char buffer[1000];
+char ch; 
+char* lines[1000] = {};
+char former;
+int lineCounter = 0;
+
+streambuf *cinbuffer = cin.rdbuf();
+ifstream file ("input.txt");
+
